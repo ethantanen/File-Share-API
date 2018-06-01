5e7d44de6abc77c02a616ba979efb4cc564e8103
Ethan Tanen <e_tanen@coloradocollege.edu>
2018-06-01 19:40:21 -0400

    initial heroku commit

--- app.js
+++ app.js
@@ -1,68 +1,107 @@
 const fs = require('fs');
 const mongodb = require('mongodb');
 const express = require('express')
+const formidable = require('formidable')
+
 const app = express()
 
+// Set the view engine in order to render interface
+app.set('view engine', 'ejs');
+
+//Middleware?
+app.use(express.static(__dirname + '/views'));
+
 // Address of mongodb hosted on mlab.com
-const uri = "mongodb://ethantanen:mississippi1@ds139920.mlab.com:39920/api_file_test";
+const uri = "mongodb://ethantanen:mississippi1@ds245240.mlab.com:45240/file-api";
 
 // GridFSBucket object used for communicating with mongodb
 var database
 
 // Connect to mongodb
-mongodb.MongoClient.connect(uri, (err, client) => {
-
+mongodb.MongoClient.connect(uri, (err, db) => {
   // Check for errors
   if(err) return console.log(err)
-
   // Make database global
-  database = new mongodb.GridFSBucket(client)
-
+  database = new mongodb.GridFSBucket(db)
   // Begin server
   app.listen(3000, () => {
     console.log('\nServer started! --> visit localhost:3000\n')
   })
-
 });
 
 
-//use /upload/* and remove upload
-app.get('/upload/files/*', (req,res) => {
+// Display form
+app.get('/', (req,res) => {
 
+  var lists = []
 
-  // Remove upload prefix from file path
-  var file_path = "." + req.url.replace("/upload","")
+  files = database.find({})
 
-  // Get file name from the end of file_path
-  var file_name = file_path.split("/").reverse()[0]
-
-  // Sanity check!
-  console.log("Uploading...","\nPath:", file_path, "\nName:", file_name)
-
-  //TODO: first idea for catching wrong files
-  fs.readFile(file_path, (err,data) => {
-    if(err) return console.log("\nFILE DOESNT EXIST")
+  files.on('data', (chunk) => {
+    console.log(chunk)
+    lists.push([chunk.filename,chunk.uploadDate,chunk._id])
   })
 
-  // Upload file to database
-  fs.createReadStream(file_path).
+  files.on('end', () => {
+    console.log("LIST: ", lists)
+    res.render('index.ejs',{list:lists})
+  })
 
-    //TODO: "Ethan" will be array of tags, also have option to include content type
-    pipe(database.openUploadStream(file_name,{"metadata":"ETHAN"})).
+})
+
+
+
+
+
+// Download file by name to current directory
+app.get('/find/name', (req,res) => {
+
+  file_name = req.query.name
+  var file = database.openDownloadStreamByName(file_name).pipe(fs.createWriteStream("./"+file_name))
+
+  res.redirect('/')
+
+})
+
+//
+
+
+
+// Upload file
+app.post('/upload', (req,res) => {
+
+  var form = new formidable.IncomingForm();
+
+  // Parse form
+  form.parse(req, function(err, fields, file) {
+
+    // Check for errors
+    if(err || file.file.name == "") return console.log("\nFile does not exists")
+
+    // Saucy print statement
+    console.log("\nUploading...","\nPath:",__dirname + file.file.path,"\nName:",file.file.name)
+
+
+    // Upload file to database
+    fs.createReadStream(file.file.path).
+
+    pipe(database.openUploadStream(file.file.name,{"metadata":fields.tags})).
 
     // Log errors
     on('error', (err)  => {
       if(err) console.log(err)
     }).
-
     // Log success message
     on('finish', function() {
-      console.log(`Success! ${file_name} stored in database`);
+      console.log(`Success! ${file.file.name} stored in database`);
+      res.redirect('/')
     });
-
-    // Redirect back to home page
-    res.redirect('/')
-
+  });
 })
 
-//TODO: add multiple, delete one/multiple, retrieve one/multiple
+
+
+
+
+
+//TODO: add multiple, delete one/multiple, retrieve one/multiple, update entries tags
