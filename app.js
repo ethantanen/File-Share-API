df4f9da9869f01db8912882f353bf90e75f8a06d
Ethan Tanen <e_tanen@coloradocollege.edu>
2018-06-03 14:23:06 -0400

    eh

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ app.post('/download/id', (req,res) => {
     dir = "/tmp"
     console.log("DIR NAME:" , __dirname + dir)
 
-    if(!fs.existsSync(dir)){
+    if(!fs.existsSync(__dirname + dir)){
       console.log("DIR CREATED")
       fs.mkdirSync(dir)
     }
