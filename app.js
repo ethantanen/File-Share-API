b27ef410f89f3775415d135260b4b4bf45588758
Ethan Tanen <e_tanen@coloradocollege.edu>
2018-06-03 16:02:35 -0400

    su[

--- app.js
+++ app.js
@@ -83,23 +83,23 @@ app.post('/download/id', (req,res) => {
 
 
     dir = "/tmp"
-    console.log("DIR NAME:" , __dirname + "/.." + dir)
+    console.log("DIR NAME:" , __dirname  + dir)
 
-    if(!fs.existsSync(__dirname + "/.." + dir)){
+    if(!fs.existsSync(__dirname + dir)){
       console.log("DIR CREATing")
-      fs.mkdirSync(__dirname +  "/.." + dir)
+      fs.mkdirSync(__dirname  + dir)
       console.log("CREATED")
     }
 
-    database.openDownloadStream(mongodb.ObjectId(fields.id)).pipe(fs.createWriteStream(__dirname + "/../tmp/" + fields.name)).
+    database.openDownloadStream(mongodb.ObjectId(fields.id)).pipe(fs.createWriteStream(__dirname + "/tmp/" + fields.name)).
     on('finish', () => {
 
 
       console.log("SOME WRITING WENT DOWN")
-      console.log(fs.existsSync(__dirname + "/../tmp/"+fields.name))
+      console.log(fs.existsSync(__dirname + "/tmp/"+fields.name))
 
 
-      res.redirect('/?link='+ __dirname + "/../tmp/" + fields.name)
+      res.redirect('/?link='+ __dirname + "/tmp/" + fields.name)
 
     })
 
