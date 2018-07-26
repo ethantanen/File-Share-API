9009e3628cfa8d0427425783706b7d987cb09aee
ethantanen <e_tanen@coloradocollege.edu>
2018-07-26 12:20:25 -0400

    added error handling and jupyter notebook

--- app.js
+++ app.js
@@ -11,9 +11,96 @@ const https = require('https')
 app = express()
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/static'))
-app.use(logger('tiny'))
+app.use(logger('dev'))
 
 const PORT = process.env.PORT || 3000
+
+// render main view
+app.get('/', (req, res) => {
+  res.render('index.ejs')
+})
+
+// upload file submitted in multipart form
+app.post('/upload',upload.single('file'), (req, res) => {
+
+  // create read stream from file
+  loc = path.join(__dirname,req.file.path)
+  readStream = fs.createReadStream(loc)
+
+  // create write stream to db w/ tags as aliases
+  writeStream = gfs.createWriteStream({
+    filename: req.file.originalname,
+    aliases: req.body.tags.split(',').map(x => x.trim()),
+  })
+
+  // pipe file to database and render homepage
+  readStream.pipe(writeStream)
+  res.render('index.ejs')
+})
+
+//download file by id
+app.get('/download/id', (req, res, next) => {
+    id = req.query.id
+    res.attachment('file')
+    gfs.createReadStream({_id: id})
+      .on('error', next)
+      .pipe(res)
+      .on('error', next)
+})
+
+// download file by name
+app.get('/download/name', (req, res, next) => {
+    name = req.query.name
+    if(!name) return next(new Error('Input valid file name!'))
+    res.attachment(name)
+    gfs.createReadStream({filename: name})
+      .on('error', next)
+      .pipe(res)
+      .on('error', next)
+})
+
+// delete file by id
+app.get('/delete/id', (req, res, next) =>{
+  id = req.query.id
+  gfs.remove({_id: id}, (err) => {
+    if (err) return next(err)
+    res.render('index.ejs')
+  })
+})
+
+// delete file by name
+app.get('/delete/name', (req, res, next) => {
+  name = req.query.name
+  if (!name) return next(new Error('Input valid file name!'))
+  gfs.remove({filename: name}, (err) => {
+    if (err) return next(err)
+    res.render('index.ejs')
+  })
+})
+
+// find file
+app.get('/find', (req, res) => {
+  query = {}
+
+  // update query object, split at commas, remove whitespace and in the case of _id convert to mongo ObjectId
+  if(req.query._id) query._id = {$in: req.query._id.split(',').map(x => x.trim()).map(x => mongo.ObjectId(x))}
+  if(req.query.filename) query.filename = {$in: req.query.filename.split(',').map(x => x.trim())}
+  if(req.query.aliases) query.aliases = {$in: req.query.aliases.split(',').map(x => x.trim())}
+
+  // query databse and return json
+  gfs.files.find(query).toArray((err,files) => {
+    if (err) return next(new Error('Invalid query!'))
+    res.status(200).json(files)
+  })
+})
+
+// render error view
+app.use((err, req, res, next) => {
+  console.log("ERROR", err)
+  res.render('error.ejs',{error: err })
+})
+
+//begin server
 app.listen(PORT, () => {
   console.log("listening on port ",PORT)
 })
@@ -33,85 +120,4 @@ mongo.MongoClient.connect(URI, async (err, db) => {
   if (err) return console.log(err)
   gfs = grid(db,mongo)
   console.log('connected to db')
-})
-
-// render main view
-app.get('/', (req, res) => {
-  res.render('index.ejs')
-})
-
-// upload file submitted in multipart form
-app.post('/upload',upload.single('file'), (req, res) => {
-
-  // block .exe and .app files
-  invalid_extensions = ['app',]
-  console.log(path.extname(req.file.originalname))
-  if(invalid_extensions.includes(path.extname(req.file.originalname))) {
-    res.status(406, "Invalid file format. Potential illegal content.")
-  }
-
-  // create read stream from file
-  loc = path.join(__dirname,req.file.path)
-  readStream = fs.createReadStream(loc)
-
-  // create write stream to db w/ tags as aliases
-  writeStream = gfs.createWriteStream({
-    filename: req.file.originalname,
-    aliases: req.body.tags.split(',').map(x => x.trim()),
-  })
-
-  // pipe file to database and render homepage
-  readStream.pipe(writeStream)
-  res.redirect('/')
-})
-
-//download file by id
-app.get('/download/id', (req, res, next) => {
-  id = req.query.id
-  res.attachment('file')
-  gfs.createReadStream({_id: id}).pipe(res)
-})
-
-// download file by name
-app.get('/download/name', (req, res, next) => {
-  name = req.query.name
-  res.attachment(name)
-  gfs.createReadStream({filename: name}).pipe(res)
-})
-
-// delete file by id
-app.get('/delete/id', (req, res, next) =>{
-  id = req.query.id
-  gfs.remove({_id:id}, (err, gridStore) => {
-    res.redirect('/')
-  })
-})
-
-// delete file by name
-app.get('/delete/name', (req, res, next) => {
-  name = req.query.name
-  gfs.remove({filename: name}, () => {
-    res.redirect('/')
-  })
-})
-
-// find file
-app.get('/find', (req, res) => {
-  query = {}
-
-  // update query object, split at commas, remove whitespace and in the case of _id convert to mongo ObjectId
-  if(req.query._id) query._id = {$in: req.query._id.split(',').map(x => x.trim()).map(x => mongo.ObjectId(x))}
-  if(req.query.filename) query.filename = {$in: req.query.filename.split(',').map(x => x.trim())}
-  if(req.query.aliases) query.aliases = {$in: req.query.aliases.split(',').map(x => x.trim())}
-
-  // query databse and return json
-  gfs.files.find(query).toArray((err,files) => {
-    res.json(files)
-  })
-})
-
-// render error view
-app.use((err, req, res, next) => {
-  console.log("ERROR", err)
-  res.render('error.ejs',{error: err })
 })
